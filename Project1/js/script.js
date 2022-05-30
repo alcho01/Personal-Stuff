a604f755e9498f0d49e73f5cf01b23923cb9bd82
alcho01 <74156727+alcho01@users.noreply.github.com>
2022-05-30 17:05:07 -0400

    first

---
+++ Project1/js/script.js
@@ -0,0 +1,28 @@
+/**
+Title of Project
+
+*/
+
+"use strict";
+
+//Canvas setup
+let canvas = {
+  width: 1000,
+  height: 1000,
+};
+
+function preload() {
+
+}
+
+function setup() {
+  userStartAudio();
+
+  createCanvas(canvas.width, canvas.height);
+
+}
+
+
+function draw() {
+  background(0);
+}
